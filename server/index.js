b3498ee6508d35f4b3527701bdf5ffb66fc21e0f
Nishith A <nishithza@gmail.com>
2020-11-22 20:53:36 +0530

    more updates

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./services/passport');
+require('./models/User')
+const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+authRoutes(app)
 
-app.get('/',(req,res)=>{
-    res.send({ hi : 'there'});
-});
-
-const PORT = process.env.PORT || 5000 ;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
